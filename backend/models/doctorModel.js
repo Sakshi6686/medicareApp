d09b60fe73fcba9df6db50cc41ea47ad8415bf70
Sakshi <sakshi6686.g@gmail.com>
2024-06-07 22:16:28 +0530

    Modified doctor model for storing location of doctors

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,61 +1,67 @@
 import mongoose from "mongoose";
 
-const doctorSchema=new mongoose.Schema({
+const doctorSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  specialization: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  feePerConsultation: {
+    type: Number,
+    required: true,
+  },
+  timings: {
+    type: Array,
+    default: true,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+}, {
+  timestamps: true,
+});
 
-    userId:{
-     type:String,
-        required:true,
-    },
-    firstName:{
-        type:String,
-        required:true,
-    },
-   lastName:{
-        type:String,
-        required:true,
-    },
-   
-phoneNumber:{
-        type:String,
-        required:true,
-    },
-  website:{
-        type:String,
-        required:true,
-    },
-    address:{
-        type:String,
-        required:true,
-    },
-    specialization:{
-        type:String,
-        required:true,
-    },
-   experience:{
-        type:String,
-        required:true,
-    },
-  feePerConsultation:{
-        type:Number,
-        required:true,
-    },
-   
-   timings:{
-    type:Array,
-    default:true,
-   },
-   status:{
-    type:String,
-    default:"pending",
-   }
+doctorSchema.index({ location: "2dsphere" });
 
-
-},
-{
-    timestamps:true,
-}
-)
-
-const doctorModel=mongoose.model("doctors",doctorSchema)
+const doctorModel = mongoose.model("doctors", doctorSchema);
 
 export default doctorModel;
