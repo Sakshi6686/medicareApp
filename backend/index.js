694f80f074a479145ba7ee9430503de9ae6f76c8
Sakshi <sakshi6686.g@gmail.com>
2024-06-30 22:39:59 +0530

    socket for video chat

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ import doctorRouter from "./router/doctorRouter.js";
 import messageRouter from './router/messageRouter.js';
 import { Server } from 'socket.io';
 import http from "http";
- 
 
 const app = express();
 app.use(express.json());
@@ -37,6 +36,7 @@ io.on('connection', (socket) => {
   console.log('A user connected', socket.id);
 
   socket.on('addUser', (userId) => {
+    console.log("usrID", userId);
     const isUserExist = users.find(user => user.userId === userId);
     if (!isUserExist) {
       const user = { userId, socketId: socket.id };
@@ -45,18 +45,62 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on("sendMessage",({senderId,receiverId,message})=>{
-          const receiver=users.find(user=>user.userId===receiverId);
-          const sender=users.find(user=>user.userId===senderId);
-          if(receiver){
-            io.to(receiver.socketId).to(sender.socketId).emit("getMessage",{
+  socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+    const receiver = users.find(user => user.userId === receiverId);
+    const sender = users.find(user => user.userId === senderId);
+    if (receiver) {
+      io.to(receiver.socketId).to(sender.socketId).emit("getMessage", {
+        senderId,
+        receiverId,
+        message
+      });
+    }
+  });
 
-              senderId,
-              receiverId,
-              message
-            })
-          }
+  // Video call signaling
+
+  // socket.on("user:call",({receiverId,offer})=>{
+  //   console.log("receiverId",receiverId);
+  //   const to = users.find(user => user.userId === receiverId);
+  //   console.log("users",users);
+  //   console.log("to",to);
+  //   io.to(to.socketId).emit("incomming-call",{from :socket.id,offer})
+  // })
+
+  // socket.on("call:accepted",({to,ans})=>{
+  //     io.to(to).emit("call:accepted",{from:socket.id,ans})
+  // })
+  socket.on("user:call", ({ receiverId, offer }) => {
+    console.log("receiverId",receiverId);
+    const to = users.find(user => user.userId === receiverId);
+    console.log("users",users);
+    console.log("to",to);
+    if (to) {
+      console.log("offer in user:call",offer);
+      io.to(to.socketId).emit("incomming-call", { from: socket.id, offer,receiverSocketId:to.socketId});
+    }
+  });
+   
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed",({to,offer})=>{
+    console.log("peer:nego:needed");
+    console.log(to);
+    console.log(offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
   })
+  socket.on("peer:nego:done",({to,ans})=>{
+    console.log("peer:nego:done");
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  })
+  socket.on("ice-candidate", ({ candidate, to }) => {
+    const receiver = users.find(user => user.socketId === to);
+    if (receiver) {
+      io.to(receiver.socketId).emit("ice-candidate", { candidate, from: socket.id });
+    }
+  });
 
   socket.on("disconnect", () => {
     users = users.filter(user => user.socketId !== socket.id);
