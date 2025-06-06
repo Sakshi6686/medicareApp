5f289b9d282735dee43863c8e0be81524314e530
Sakshi6686 <104898310+Sakshi6686@users.noreply.github.com>
2025-06-06 19:07:23 +0530

    Update ProtectedRoute.js

--- frontend/src/component/ProtectedRoute.js
+++ frontend/src/component/ProtectedRoute.js
@@ -11,13 +11,14 @@ import { hideLoading, showLoading } from '../redux/alertSlice';
 const ProtectedRoute = (props) => {
 const {user}=useSelector((state)=>state.user)
 console.log(user);
+     const url="https://medicareapp-backend.onrender.com"
 const navigate=useNavigate();
 const dispatch=useDispatch();;
 const getUser=async()=>{
 
     try{
         dispatch(showLoading());
-        const res=await axios.post("/api/user/get-user-info-by-id",{token:localStorage.getItem("token")},
+        const res=await axios.post(url+"/api/user/get-user-info-by-id",{token:localStorage.getItem("token")},
         {headers:{
             Authorization:`Bearer ${localStorage.getItem("token")}`,
         },
