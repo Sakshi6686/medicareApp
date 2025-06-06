d02c060a7f41454c1cd51440c206c34dd8e6c4e9
Sakshi6686 <104898310+Sakshi6686@users.noreply.github.com>
2025-06-06 19:10:35 +0530

    Update SocketProvider.jsx

--- frontend/src/context/SocketProvider.jsx
+++ frontend/src/context/SocketProvider.jsx
@@ -2,14 +2,14 @@ import React, { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
-
+const url="https://medicareapp-backend.onrender.com"
 export const useSocket = () => {
   const socket = useContext(SocketContext);
   return socket;
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://medicareapp-backend.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
